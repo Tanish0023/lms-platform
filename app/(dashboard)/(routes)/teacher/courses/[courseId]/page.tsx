3079c09357594a3eeb3131080e6a22d51f83ccd1
Tanish0023 <tanish23aggarwal@gmail.com>
2024-09-16 23:30:02 +0530

    Added feature in /teacher/courses/[courseId] - To get course title and decription

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,8 +1,13 @@
-import { IconBadge } from "@/components/icon-badge";
-import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs/server";
 import { LayoutDashboard } from "lucide-react";
-import { redirect } from "next/navigation";
+
+import { IconBadge } from "@/components/icon-badge";
+import { db } from "@/lib/db";
+
+// from _components
+import { TitleForm } from "./_components/title-form";
+import { DescriptionForm } from "./_components/description-form";
 
 const CourseIdPage = async ({params}:{
     params:{courseId:string}
@@ -64,6 +69,14 @@ const CourseIdPage = async ({params}:{
                             Customize your course
                         </h2>
                     </div>
+                    <TitleForm
+                        initialData={course}
+                        courseId={course.id}
+                    />
+                    <DescriptionForm
+                        initialData={course}
+                        courseId={course.id}
+                    />
                 </div>
             </div>
         </div>
