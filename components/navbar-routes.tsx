8f430d041925501aff702a2bb0af4fd5236877f0
Tanish0023 <tanish23aggarwal@gmail.com>
2024-10-12 22:22:39 +0530

    Browse Tab - Search Query

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -5,6 +5,7 @@ import { usePathname } from "next/navigation"
 import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
+import { SearchInput } from "./seach-input";
 
 export const NavbarRoutes = ()=>{
     const pathName = usePathname();
@@ -15,24 +16,33 @@ export const NavbarRoutes = ()=>{
     // Used to display chapters in the sidebar
     const isPlayerPage = pathName?.includes("/chapter")
 
+    const isSearchPage = pathName === "/search"
+
     return(
-        <div className="flex gap-x-2 ml-auto">
-            {isTeacherPage || isPlayerPage ? (
-                <Link href="/">
-                    <Button size="sm" variant="ghost">
-                        <LogOut className="h-4 w-4 mr-2"/>
-                        Exit
-                    </Button>
-                </Link>
-            ):(
-                <Link href="/teacher/courses">
-                    <Button size="sm" variant="ghost">
-                        Teacher Mode
-                    </Button>
-                </Link>
+        <>
+            {isSearchPage && (
+                <div className="hidden md:block">
+                    <SearchInput/>
+                </div>
             )}
-            <UserButton/>
-        </div>
+            <div className="flex gap-x-2 ml-auto">
+                {isTeacherPage || isPlayerPage ? (
+                    <Link href="/">
+                        <Button size="sm" variant="ghost">
+                            <LogOut className="h-4 w-4 mr-2"/>
+                            Exit
+                        </Button>
+                    </Link>
+                ):(
+                    <Link href="/teacher/courses">
+                        <Button size="sm" variant="ghost">
+                            Teacher Mode
+                        </Button>
+                    </Link>
+                )}
+                <UserButton/>
+            </div>
+        </>
     )
 
 }
