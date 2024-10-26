f77a2c60f576498b63dfe5c4cde45bf384813ed1
Tanish0023 <tanish23aggarwal@gmail.com>
2024-10-26 11:50:51 +0530

    Correct Break Points

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,11 @@ module.exports = withUt({
   ],
   theme: {
     screens: {
+      sm: "640px",
       md: "840px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
 
     extend: {
